8c52cca983fd00308dc7ef04cb10bab22b486c90
Tomas Bencomo <tjbencomo@gmail.com>
2022-02-08 12:09:03 -0800

    Fixes

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,15 +2,15 @@
 #SBATCH --job-name=
 #SBATCH --output=
 #SBATCH --nodes=1
-#SBATCH --time=
+#SBATCH --time=01-12:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=
+#SBATCH --mem=300
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=
 
 
 set -e
 cd $(pwd) #Must be run from snakemake directory
-snakemake --cluster-config cluster.wes.json -j 499 \
+snakemake --cluster-config cluster.json -j 499 \
     --use-singularity \
     --cluster 'sbatch -p {cluster.partition} -t {cluster.time} --mem {cluster.mem} -c {cluster.ncpus} -o {cluster.out}'
